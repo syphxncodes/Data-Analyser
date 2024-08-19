32b7bd3738e972795b912f6938a8182c70707c0d
LmG Syphxn <f20230254@dubai.bits-pilani.ac.in>
2024-08-19 11:47:54 +0400

    added

--- Model_Training.py
+++ Model_Training.py
@@ -13,9 +13,19 @@ if selection == 'y':
         df.drop(columns=column,inplace=True)
         print("The column is removed, here is the dataframe information:")
         print(df.info())
+else:
+    pass
 
 
 print("Now we will come to train test split")
 
 def ModelTraining():
-    pass
+    input1=input("Enter the column that needs to be trained:")
+    x=df.drop(input1,inplace=True)
+    y=df[f"{input1}"]
+    random_state_input=int(input("Enter the random state required:"))
+    xtrain,xtest,ytrain,ytest=train_test_split(x,y,random_state=random_state_input)
+    print("Train Test Split is done")
+
+
+ModelTraining()
