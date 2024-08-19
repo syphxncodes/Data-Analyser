8c10ae6040547ee82abef61b9f84f8f537e215e3
LmG Syphxn <f20230254@dubai.bits-pilani.ac.in>
2024-08-19 16:01:17 +0400

    added model training

--- Model_Training.py
+++ Model_Training.py
@@ -2,6 +2,17 @@ import numpy as np
 import pandas as pd
 from filling_null_values import nullvalues
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+#import xgboost as xgb
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
 
 df=nullvalues()
 selection=input("Based on the statistics that you viewed, do u want to remove any column(y/n):")
@@ -20,12 +31,25 @@ else:
 print("Now we will come to train test split")
 
 def ModelTraining():
-    input1=input("Enter the column that needs to be trained:")
-    x=df.drop(input1,inplace=True)
-    y=df[f"{input1}"]
+    input1=input("Enter the column that needs to be trained:").replace("'","").replace('"',"")
+    x=df.drop(columns=[input1])
+    y=df[input1]
     random_state_input=int(input("Enter the random state required:"))
     xtrain,xtest,ytrain,ytest=train_test_split(x,y,random_state=random_state_input)
     print("Train Test Split is done")
-
+    print(y.unique())
+    input1=input("Please type what problem it is based on(Classification or Regression):")
+    if input1 == "Classification":
+        print("The default metric for classification used is accuracy")
+        models=[LogisticRegression(),KNeighborsClassifier(),DecisionTreeClassifier(),GradientBoostingClassifier(),RandomForestClassifier(),GaussianNB(),MLPClassifier(),AdaBoostClassifier()]
+        #models=KNeighborsClassifier()
+        accuracies={}
+        for i in models:
+            model=i
+            model.fit(xtrain,ytrain)
+            y_pred=model.predict(xtest)
+            accuracy=accuracy_score(ytest,y_pred)
+            accuracies[model]=accuracy
+        print(accuracies)
 
 ModelTraining()
